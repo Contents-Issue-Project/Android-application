52ed5ba169d568ad37c3abc070b6082d6670f23f
Go-yj <ko96203yj@gamil.com>
2020-10-16 20:20:40 +0900

    bookmark 로그인 했을 때, 안했을 때 화면 변화

--- app/src/main/java/com/example/android_application/presentation/Home/Home_Fragment.java
+++ app/src/main/java/com/example/android_application/presentation/Home/Home_Fragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +14,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android_application.Data.Bookmark.BookmarkParam;
 import com.example.android_application.Data.New.NewParam;
 import com.example.android_application.Data.Trending.TrendingParam;
 import com.example.android_application.MainActivity;
 import com.example.android_application.R;
+import com.example.android_application.presentation.Home.Bookmark.BookmarkAdapter;
+import com.example.android_application.presentation.Home.Bookmark.BookmarkContract;
+import com.example.android_application.presentation.Home.Bookmark.BookmarkPresenter;
 import com.example.android_application.presentation.Home.Bookmark.Bookmark_Fragment;
 import com.example.android_application.presentation.Home.New.NewAdapter;
 import com.example.android_application.presentation.Home.New.NewAllFragment;
@@ -33,18 +38,24 @@ import java.util.ArrayList;
 public class Home_Fragment extends Fragment{
     private TrendingContract.Presenter trendingPresenter;
     private NewContract.Presenter newPresenter;
+    private BookmarkContract.Presenter bookmarkPresenter;
 
     // private View view;
 
     private RecyclerView trending_recyclerView;
     private RecyclerView new_recyclerView;
+    private RecyclerView bookmark_recyclerView;
     private TrendingAdapter trending_adapter;
     private NewAdapter new_adapter;
+    private BookmarkAdapter bookmark_adapter;
     private ArrayList<ItemData> trending_list = new ArrayList<>();
     private ArrayList<ItemData> new_list = new ArrayList<>();
+    private ArrayList<ItemData> bookmark_list = new ArrayList<>();
     private Button trending_button;
     private Button new_button;
     private Button bookmark_button;
+    private FrameLayout bookmark_login;
+    private FrameLayout bookmark_logout;
 
     public static Home_Fragment newInstance() {
         return new Home_Fragment();
@@ -58,9 +69,12 @@ public class Home_Fragment extends Fragment{
 
         trending_recyclerView = (RecyclerView) view.findViewById(R.id.Trending_recycler);
         new_recyclerView = (RecyclerView) view.findViewById(R.id.New_recycler);
+        bookmark_recyclerView = (RecyclerView) view.findViewById(R.id.Bookmark_recycler);
         trending_button = (Button) view.findViewById(R.id.trending_button);
         new_button = (Button) view.findViewById(R.id.new_button);
         bookmark_button = (Button) view.findViewById(R.id.bookmark_button);
+        bookmark_login = (FrameLayout) view.findViewById(R.id.bookmark_login);
+        bookmark_logout = (FrameLayout) view.findViewById(R.id.bookmark_logout);
 
         trending_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,6 +96,9 @@ public class Home_Fragment extends Fragment{
                 ((MainActivity)getActivity()).replaceFragment(Bookmark_Fragment.newInstance());
             }
         });
+
+        bookmark_login.setVisibility(View.VISIBLE);
+        //bookmark_logout.setVisibility(View.VISIBLE);
 
         //trending_list = ItemData.createContactsList(10);
         trending_recyclerView.setHasFixedSize(true);
@@ -106,7 +123,7 @@ public class Home_Fragment extends Fragment{
         new_param.result_count = 10;
         newPresenter.loadNew(new_param);
 
-/*      bookmark_recyclerView.setHasFixedSize(true);
+        bookmark_recyclerView.setHasFixedSize(true);
         bookmark_adapter = new BookmarkAdapter(getActivity(), bookmark_list);
         bookmark_recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
         bookmark_recyclerView.setAdapter(bookmark_adapter);
@@ -114,7 +131,7 @@ public class Home_Fragment extends Fragment{
         bookmarkPresenter = new BookmarkPresenter(bookmark_adapter);
         BookmarkParam bookmark_param = new BookmarkParam();
         bookmark_param.result_count = 10;
-        bookmarkPresenter.loadBookmark(bookmark_param);*/
+        bookmarkPresenter.loadBookmark(bookmark_param);
 
 
         Log.e("Frag", "HomeFragment");
